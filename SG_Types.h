bde61650d8b28804d36dbf135b1b915fb924fc86
Mark Chaisson <mchaisso@uw.edu>
2018-01-14 16:10:47 -0800

    missing header

---
+++ SG_Types.h
@@ -0,0 +1,12 @@
+#ifndef SG_TYPES_H_
+#define SG_TYPES_H_
+
+typedef unsigned long Word;
+typedef unsigned char AdjacencyList;
+typedef unsigned char Byte; 
+typedef unsigned char Nuc;
+
+
+
+
+#endif
